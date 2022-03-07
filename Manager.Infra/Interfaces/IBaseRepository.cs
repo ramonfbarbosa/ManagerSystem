62f7b788be214f407f723b68e197d936f5c4ee5c
ramonfbarbosa <ramonmfb777@gmail.com>
2022-03-07 14:59:37 -0300

    Base repository

--- Manager.Infra/Interfaces/IBaseRepository.cs
+++ Manager.Infra/Interfaces/IBaseRepository.cs
@@ -6,10 +6,11 @@ namespace Manager.Infra.Interfaces
 {
     public interface IBaseRepository<T> where T : Base
     {
+        Task<T> Get(long id);
+        Task<List<T>> Get();
         Task<T> Create(T obj);
         Task<T> Update(T obj);
         Task Delete(long id);
-        Task<T> Get(long id);
-        Task<List<T>> Get();
+        
     }
 }
